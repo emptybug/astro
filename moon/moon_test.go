38eea4a0ab40d705a9d49dbc23ead2ec48b805f2
emptybug <emptybug@outlook.com>
2020-09-06 17:29:03 +0800

    🌙 增加测试用例：月亮升起落下

--- moon/moon_test.go
+++ moon/moon_test.go
@@ -3,12 +3,23 @@ package moon
 import (
 	"fmt"
 	"testing"
+	"time"
+
+	"github.com/starainrt/astro/basic"
 )
 
 func TestMoonI(t *testing.T) {
-	fmt.Println(MoonR(2465445.9755443))
+	fmt.Println(basic.MoonR(2465445.9755443))
 }
 
 func Test_NewCalc(t *testing.T) {
-	fmt.Printf("%.14f", MoonCalcNew(2, 2451546.0))
+	fmt.Printf("%.14f", basic.MoonCalcNew(2, 2451546.0))
+}
+
+func TestDownTime(t *testing.T) {
+	fmt.Println(DownTime(time.Now(), 114, 21, 8, false))
+}
+
+func TestRiseTime(t *testing.T) {
+	fmt.Println(RiseTime(time.Now(), 114, 21, 8, false))
 }
